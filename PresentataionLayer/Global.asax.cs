f20d00243c7a41cc0526297e2225e6a000f3e1e2
alexeim <alexei.mel@gmail.com>
2016-10-11 13:18:55 +0300

    Added PresentationLayer solution

---
+++ PresentataionLayer/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PresentataionLayer
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomViewEngine());
+        }
+
+        public class CustomViewEngine : RazorViewEngine
+        {
+            public CustomViewEngine()
+            {
+                var viewLocations = new[]
+                {
+                    "~/Presentation/Views/{1}/{0}.cshtml",
+                    "~/Presentation/Views/Shared/{0}.cshtml"
+                };
+
+                this.PartialViewLocationFormats = viewLocations;
+                this.ViewLocationFormats = viewLocations;
+            }
+        }
+
+    }
+}
